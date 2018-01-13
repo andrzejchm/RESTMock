8db29ca8bb7c5b0861a3654610ccfa96b19a6b09
Andrzej Chmielewski <andrzejchm@gmail.com>
2018-01-13 20:40:26 +0100

    #71 add 'thenAnswer' method to allow response building on runtime

--- core/src/main/java/io/appflate/restmock/MatchableCall.java
+++ core/src/main/java/io/appflate/restmock/MatchableCall.java
@@ -23,13 +23,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import io.appflate.restmock.utils.RequestMatcher;
 import io.appflate.restmock.utils.RestMockUtils;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.RecordedRequest;
 
 /**
- * Represents a Http call with the {@link MockResponse} to be returned for a HTTP request matched by {@link
- * io.appflate.restmock.utils.RequestMatcher RequestMatcher}.
+ * Represents a Http call with the {@link MockAnswer} to be returned for a HTTP request matched by {@link RequestMatcher}.
  * In order to create new {@code MatchableCall} call  one of the {@link RESTMockServer} methods.
  */
 public class MatchableCall {
@@ -37,7 +37,7 @@ public class MatchableCall {
     final Matcher<RecordedRequest> requestMatcher;
     private final RESTMockFileParser RESTMockFileParser;
     private MatchableCallsRequestDispatcher dispatcher;
-    private List<MockResponse> responses;
+    private List<MockAnswer> answers;
     private List<Long> delays;
     private int responseIndex;
 
@@ -46,7 +46,7 @@ public class MatchableCall {
         this.RESTMockFileParser = RESTMockFileParser;
         this.requestMatcher = requestMatcher;
         this.dispatcher = dispatcher;
-        this.responses = new LinkedList<>();
+        this.answers = new LinkedList<>();
         this.delays = new LinkedList<>();
         this.responseIndex = 0;
     }
@@ -100,30 +100,6 @@ public class MatchableCall {
      */
     public MatchableCall thenReturnEmpty(int responseCode) {
         return thenReturnString(responseCode, "");
-    }
-
-    /**
-     * Makes this {@code MatchableCall} return  {@link MockResponse}
-     *
-     * <p>This {@code MatchableCall} will be automatically scheduled within the {@code RESTMockServer} if you want to prevent that, see
-     * {@link MatchableCall#dontSet()}</p>
-     *
-     * <p>If you specify more than one response, each consecutive call to server will return next response from the list, if number of
-     * requests exceeds number of specified responses, the last response will be repeated</p>
-     *
-     * @param mockResponses a {@link MockResponse} that will be returned with this {@code MatchableCall}
-     * @return this {@code MatchableCall}
-     */
-    public MatchableCall thenReturn(MockResponse... mockResponses) {
-        if (mockResponses != null) {
-            for (MockResponse response : mockResponses) {
-                if (response != null) {
-                    this.responses.add(response);
-                }
-            }
-        }
-        addToDispatcher();
-        return this;
     }
 
     /**
@@ -196,30 +172,76 @@ public class MatchableCall {
         return this;
     }
 
-    MockResponse response() {
-        MockResponse mockResponse = peekResponse();
+    /**
+     * Makes this {@code MatchableCall} return  {@link MockResponse}
+     *
+     * <p>This {@code MatchableCall} will be automatically scheduled within the {@code RESTMockServer} if you want to prevent that, see
+     * {@link MatchableCall#dontSet()}</p>
+     *
+     * <p>If you specify more than one response, each consecutive call to server will return next response from the list, if number of
+     * requests exceeds number of specified responses, the last response will be repeated</p>
+     *
+     * @param mockResponses a {@link MockResponse} that will be returned with this {@code MatchableCall}
+     * @return this {@code MatchableCall}
+     */
+    public MatchableCall thenReturn(MockResponse... mockResponses) {
+        if (mockResponses != null) {
+            MockAnswer[] mockAnswers = new MockAnswer[mockResponses.length];
+            int i = 0;
+            for (MockResponse response : mockResponses) {
+                if (response != null) {
+                    mockAnswers[i++] = new SimpleMockResponseAnswer(response);
+                }
+            }
+            thenAnswer(mockAnswers);
+        }
+        return this;
+    }
+
+    /**
+     * Makes this {@code MatchableCall} return  {@link MockAnswer}
+     *
+     * <p>This {@code MatchableCall} will be automatically scheduled within the {@code RESTMockServer} if you want to prevent that, see
+     * {@link MatchableCall#dontSet()}</p>
+     *
+     * <p>If you specify more than one answer, each consecutive call to server will call next answer from the list, if number of
+     * requests exceeds number of specified answers, the last answer will be repeated</p>
+     *
+     * @param mockAnswers a {@link MockAnswer} that will be triggered with a matched {@link RecordedRequest}
+     * @return this {@code MatchableCall}
+     */
+    public MatchableCall thenAnswer(MockAnswer... mockAnswers) {
+        if (mockAnswers != null) {
+            for (MockAnswer answer : mockAnswers) {
+                if (answer != null) {
+                    this.answers.add(answer);
+                }
+            }
+        }
+        addToDispatcher();
+        return this;
+    }
+
+    MockResponse nextResponse(RecordedRequest request) {
+        MockResponse mockResponse;
+        if (answers.isEmpty()) {
+            mockResponse = null;
+        } else {
+            mockResponse = currentResponse(request);
+            setResponseDelayInternal(mockResponse);
+        }
         responseIndex++;
         return mockResponse;
     }
 
-    MockResponse peekResponse() {
-        if (responses.isEmpty()) {
-            return null;
+    private MockResponse currentResponse(RecordedRequest request) {
+        MockAnswer mockAnswer;
+        if (responseIndex >= answers.size()) {
+            mockAnswer = answers.get(answers.size() - 1);
         } else {
-            MockResponse response = currentResponseInternal();
-            setResponseDelayInternal(response);
-            return response;
+            mockAnswer = answers.get(responseIndex);
         }
-    }
-
-    private MockResponse currentResponseInternal() {
-        MockResponse response;
-        if (responseIndex >= responses.size()) {
-            response = responses.get(responses.size() - 1);
-        } else {
-            response = responses.get(responseIndex);
-        }
-        return response;
+        return mockAnswer.answer(request);
     }
 
     private void setResponseDelayInternal(MockResponse response) {
@@ -234,11 +256,25 @@ public class MatchableCall {
         response.setBodyDelay(delay, TimeUnit.MILLISECONDS);
     }
 
-    List<MockResponse> getResponses() {
-        return responses;
+    int getNumberOfAnswers() {
+        return answers.size();
     }
 
     private void addToDispatcher() {
         dispatcher.addMatchableCall(this);
+    }
+
+    private static class SimpleMockResponseAnswer implements MockAnswer {
+
+        private final MockResponse response;
+
+        public SimpleMockResponseAnswer(MockResponse response) {
+            this.response = response;
+        }
+
+        @Override
+        public MockResponse answer(RecordedRequest request) {
+            return response;
+        }
     }
 }
